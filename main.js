e760140b15bb81b91324cec9990b09bcb949f8ef
Sandra Nwakeze <sandramakis.dev@gmail.com>
2023-09-30 03:13:26 +0100

    All files

---
+++ main.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const inputs = document.querySelectorAll(".controls input");
+
+function handleUpdates() {
+  const suffix = this.dataset.sizing || "";
+
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  );
+  console.log(this.value + suffix);
+}
+
+inputs.forEach((input) => input.addEventListener("change", handleUpdates));
+inputs.forEach((input) => input.addEventListener("mousemove", handleUpdates));
